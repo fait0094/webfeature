dfc4442fe48240885fe7f7644c45c09bd17515d2
Faith Mansilungan <faith0094@stud.kea.dk>
2023-02-21 15:19:08 +0100

    Horisontal scroll

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
-
 animate("#cameralens4", { opacity: [1, 0] }, { delay: 2, duration: 0.15 });
 
 animate("#cameralens3", { opacity: [1, 0] }, { delay: 2.5, duration: 0.15 });
@@ -10,6 +9,14 @@ animate("#cameralens2", { opacity: [1, 0] }, { delay: 3, duration: 0.15 });
 
 animate(
   "#titel",
-  { opacity: [0,1], scale:[0.5, 0.75, 1] },
-  {delay:3.5, duration: 1,}
+  { opacity: [0, 1], scale: [0.5, 0.75, 1] },
+  { delay: 3.5, duration: 1 }
+);
+
+const items = document.querySelectorAll("#horisontalliste li");
+scroll(
+  animate("#horisontalliste", {
+    transform: ["none", `translateX(-${items.length - 1}00vw)`],
+  }),
+  { target: document.querySelector("#horisontalscrollsection") }
 );
