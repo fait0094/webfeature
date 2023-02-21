0bda64e8e1d0bb3a314c2036726c97e5cc308b10
mgcviii <111751220+mgcviii@users.noreply.github.com>
2023-02-21 14:10:02 +0100

    anime

--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+// animate(
+//     "div.lens",
+//     { opacity: [1, 0 ] },
+//     {
+//         delay: stagger(1, { start: 2 })
+//     },
+//     { direction: "reverse" },
+//   )
+
+animate("#cameralens4", { opacity: [1, 0] }, { delay: 2, duration: 0.15 });
+
+animate("#cameralens3", { opacity: [1, 0] }, { delay: 2.5, duration: 0.15 });
+
+animate("#cameralens2", { opacity: [1, 0] }, { delay: 3, duration: 0.15 });
+
+animate(
+  "#titel",
+  { opacity: [0,1], scale:[0.5, 0.75, 1] },
+  {delay:3.5, duration: 1,}
+);
