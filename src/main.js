aee4db4f6601911ed957bb32f0576b02c0112197
mgcviii <111751220+mgcviii@users.noreply.github.com>
2023-02-24 05:26:57 +0100

    lidt rettelser

--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
+
+// hero animations//
+// camera lens //
 animate("#cameralens4", { opacity: [1, 0] }, { delay: 2, duration: 0.15 });
 
 animate("#cameralens3", { opacity: [1, 0] }, { delay: 2.5, duration: 0.15 });
 
 animate("#cameralens2", { opacity: [1, 0] }, { delay: 3, duration: 0.15 });
 
+// h1 titel //
 animate(
   "#titel",
   { opacity: [0, 1], scale: [0.5, 0.75, 1] },
   { delay: 3.5, duration: 1 }
 );
 
+//blitz//
 animate(".flash", { opacity: [0, 1, 0] }, { delay: 3.51, duration: 1 });
+
+//scroll icon
+animate("#scrollicon", { opacity: [0, 1] }, { delay: 3.51, duration: 1 });
+
+
+
 
 const items = document.querySelectorAll("#horisontalliste li");
 scroll(
@@ -40,25 +51,24 @@ scroll(
 );
 
 inView(".contentinview1", () => {
-  animate(".contentinview1", { x: [-2000, 0] }, { delay: 0.5, duration: 1 });
+  animate(".contentinview1", { x: [-1000, 0] }, { delay: 0.75, duration: 1.5 });
 });
 
 inView(".contentinview2", () => {
-  animate(".contentinview2", { x: [2000, 0] }, { delay: 0.5, duration: 1 });
+  animate(".contentinview2", { x: [1000, 0] }, { delay: 0.75, duration: 1.5 });
 });
 
 inView(".contentinview3", () => {
-  animate(".contentinview3", { x: [-2000, 0] }, { delay: 0.5, duration: 1 });
+  animate(".contentinview3", { x: [-1000, 0] }, { delay: 0.75, duration: 1.5 });
 });
 
-scroll(
+inView(
   ".citat",
   animate(
     ".citat",
     { opacity: [0, 1] },
     {
-      delay: stagger(1, { start: 2  }),
-    },
-    { repeat: Infinity }
+      delay: stagger(1.5, { start: 2  }),
+    }
   )
 );
